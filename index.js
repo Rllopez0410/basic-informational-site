bb06e52d552388772628175deaebf6af3a51238f
Roger Lopez <rllopez0410@gmail.com>
2024-04-12 16:05:53 -0400

    server reads err file it pathname/file is not found

--- index.js
+++ index.js
@@ -7,15 +7,21 @@ http.createServer(function(req, res) {
     let fileName = "." + q.pathname;
     fs.readFile(fileName, function(err, data) {
         if (err) {
-            res.writeHead(404, {"Content-Type": "text/html"});
-            q.pathname = "err.html"
-            res.write(q);
-            return res.end(); 
+            if (err.code === 'ENOENT') {
+                // File not found, serve err.html
+                fs.readFile("./err.html", function(err, data) {
+                    if (err) throw err;
+                    res.writeHead(404, {"Content-Type": "text/html"});
+                    res.write(data);
+                    return res.end();
+                });
+            } 
+        } else {
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.write(data);
+            return res.end();
         }
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.write(data);
-        return res.end();
-    })
-}).listen(8080)
+    });
+}).listen(8080);
 
 console.log("Server is now listening on port: 8080...");
